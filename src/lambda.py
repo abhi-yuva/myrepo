1d73b2e0ef6db3a07b6fb4222d053c10a5b55c92
abhi-yuva <95558376+abhi-yuva@users.noreply.github.com>
2021-12-05 10:44:43 +0530

    Update lambda.py

--- src/lambda.py
+++ src/lambda.py
@@ -44,11 +44,11 @@ count =0
 #def ses_notification():
     # Replace sender@example.com with your "From" address.
 	# This address must be verified with Amazon SES.
-SENDER = "abhilash.kumar.kopparapu@hp.com"
+SENDER = "abhilashkumar.kopparapu@gmail.com"
 
 	# Replace recipient@example.com with a "To" address. If your account 
 	# is still in the sandbox, this address must be verified.
-RECIPIENT = "abhilash.kumar.kopparapu@hp.com"
+RECIPIENT = "abhilashkumar.kopparapu@gmail.com"
 
 	# Specify a configuration set. If you do not want to use a configuration
 	# set, comment the following variable, and the 
